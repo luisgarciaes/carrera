6b9ec13e2ba130b691ba903c6992bb527cbc0efd
Zaddoz Garcia <zaddozgl@gmail.com>
2021-08-31 20:46:09 -0500

    converted to one button synchronous race

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ let playerA = 0;
 let playerB = 0;
 let advBtn = document.getElementById("advanceBtn")
 let result;
+let moveInterval;
+let raceInterval;
 
+const createInterval = () => {
+    raceInterval = setInterval(race,200);
+    moveInterval = setInterval(move,200)
+    advBtn.disabled=true
+}
 
 const avanzar = () => {
     let random = 0;
@@ -22,29 +29,32 @@ const checkWinner = () =>{
         result= "Es un empate"
         document.getElementById("winner").innerHTML=result;
         advBtn.disabled=true
+        clearInterval(raceInterval);
+        clearInterval(moveInterval);
         console.log(result)
     }else if(playerA >= 100 || playerB >= 100){
         if(playerA > playerB){
         result="JUGADOR A GANA!"
         document.getElementById("winner").innerHTML=result;
-        advBtn.disabled=true
+        clearInterval(raceInterval);
+        clearInterval(moveInterval);
         console.log(result)
         }else{
         result="JUGADOR B GANA!"}
         document.getElementById("winner").innerHTML=result;
-        advBtn.disabled=true
+        clearInterval(raceInterval);
+        clearInterval(moveInterval);
         console.log(result)
-    }   
+    }   else{
+    }
+    
 }
-const carrera = () =>{
-
+const race = () =>{
     checkWinner()
     playerA += avanzar();
     console.log("El jugador A avanza hasta " + playerA)
     playerB += avanzar()
     console.log("El jugador B avanza hasta " + playerB)
-    checkWinner()
-
     return result;
 }
 const move = () => {
@@ -57,6 +67,5 @@ const move = () => {
     elemA.innerHTML = widthA * 1  + '%'
     elemB.innerHTML = widthB * 1  + '%'
   }
-document.getElementById("advanceBtn").addEventListener("click", carrera); 
-document.getElementById("advanceBtn").addEventListener("click", move); 
+
 
